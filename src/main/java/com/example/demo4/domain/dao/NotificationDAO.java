b02bf0e9f5d3fd62473974fcfd411d9f8ad706b9
JeffreyDambre <jeffrey.dambre@gmail.com>
2020-02-04 12:26:54 +0100

    more exhaustive test

--- src/main/java/com/example/demo4/domain/dao/NotificationDAO.java
+++ src/main/java/com/example/demo4/domain/dao/NotificationDAO.java
@@ -3,6 +3,9 @@ package com.example.demo4.domain.dao;
 import com.example.demo4.domain.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * {@link Notification} DAO
+ */
 public interface NotificationDAO extends JpaRepository<Notification, Integer> {
 }
 
